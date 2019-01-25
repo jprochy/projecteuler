ec830f854ffcd3a94892c9c6687212086454136f
Jan Prochazka <jan.prochazka@mail.com>
2019-01-25 14:37:25 +0100

    p055

---
+++ p055_lychrel_numbers.py
@@ -0,0 +1,17 @@
+import time
+start = time.time()
+
+
+def is_lychrel(n):
+    for i in range(50):
+        number = n + int(str(n)[::-1])
+        if str(number) == str(number)[::-1]:
+            return False
+        n = number
+    return True
+
+result = [is_lychrel(num) for num in range(10001)].count(True)
+
+print(f"Result: {result}")
+end = time.time()
+print("Time: {0:.4f} ms".format(end - start))
