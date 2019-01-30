62432f1c5e042236eae5c9e82942676ab05cb17b
Jan Prochazka <jan.prochazka@mail.com>
2019-01-30 21:13:13 +0100

    p097

--- 051-100/p097_large_nonmersenne_prime.py
+++ 051-100/p097_large_nonmersenne_prime.py
@@ -1,16 +1,17 @@
 import time
+from gmpy2 import mpz
 start = time.time()
 
 MOD = 10000000000
 EXP_MAX = 7830456
-BASE = 2
+EXP = 2
 MULT = 28433
 
-exp = 2
+base = 2
 for i in range(EXP_MAX):
-    exp = (BASE * exp) % MOD
+    base = (base * EXP) % MOD
 
-result = (exp * MULT + 1) % MOD
+result = (base * MULT + 1) % MOD
 
 print(f"Result: {result}")
 end = time.time()
